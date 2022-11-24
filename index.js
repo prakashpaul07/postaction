e5e9a9bf856724fd0984af9230cc7e17bff33397
saikumar3 <saikumarbonthu9@gmail.com>
2022-11-24 12:25:37 +0530

    24nov

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const app=express()
+const port=4000
+const bodyParser=require("body-parser")
+const cors=require("cors")
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+
+app.get("/",(req,res)=>{
+	res.send("hi im a server 4.2 version")
+})
+
+app.post("/newData",(req,res)=>{
+	//console.log(req.body)
+	const {name,age,password}=req.body
+	console.log(name,password,age,"values")
+})
+
+
+app.listen(port,()=>console.log("server is working"))
